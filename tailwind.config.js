3fca87db435c509c3e3c2fadfe0e1a587e94b6d0
AkhmadFaiz012 <akhmadf109@gmail.com>
2023-12-02 02:59:09 +0700

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "/src/html/page.html"],
+  content: ["index.html", "./src/html/page.html"],
   theme: {
     extend: {
       colors: {
